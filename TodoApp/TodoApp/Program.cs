d37501343038cefb4e7b696f11afd3ba3c1aaa4a
krupspurohit <krupapurihit99@gmail.com>
2026-08-11 16:55:21 +0530

    Created Home Page

--- TodoApp/TodoApp/Program.cs
+++ TodoApp/TodoApp/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Todo List");
+Console.ResetColor();
+Console.WriteLine();
 
+Console.Write("Enter Your name: ");
+var name = Console.ReadLine();
+Console.WriteLine();
+
+Console.WriteLine("Your name: " + name );
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("-------- Your TodoList --------");
+Console.ResetColor();
+
+Console.WriteLine();
+Console.WriteLine("Display todo list");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.DarkGray;
+Console.WriteLine("------------------------");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("1) AddTodo");
+Console.WriteLine("2) DeleteTodo");
+Console.WriteLine("3) CompleteTodo");
+Console.WriteLine("4) UpdateTodo");
+Console.WriteLine("5) Exit");
+Console.ResetColor();
---
+++ TodoApp/TodoApp/TodoActions/TodoActions.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace TodoApp.TodoActions
+{
+    public class TodoActions
+    {
+        public int Id {  get; set; }
+        public string Title {  get; set; }
+        public string Description {  get; set; }
+
+        public string DisplayTodoList()
+        {
+            return $"{Id} {Title} {Description}";
+        }
+
+        public void AddTodo(string title, string description)
+        {
+
+        }
+
+        public void UpdateTodo()
+        {
+
+        }
+
+        public void DeleteTodo()
+        {
+
+        }
+    }
+}
