f9412d488a309ee00954596f1f1b8a98fb192ebf
krupspurohit <krupapurihit99@gmail.com>
2026-08-19 15:04:24 +0530

    Done Add Todo && Complete Todo

---
+++ TodoApp/TodoApp/Pages/AddTodo.cs
@@ -0,0 +1,24 @@
+﻿using TodoApp.Models;
+
+namespace TodoApp.Pages
+{
+    public class AddTodo
+    {
+        public static Todo CreateTodo()
+        {
+            Console.Write("Add Your Todo Title: ");
+            var getTitle = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.Write("Add Your Todo Description: ");
+            var getDesciption = Console.ReadLine();
+
+            if(!string.IsNullOrEmpty(getTitle))
+            {
+                return new Todo(getTitle, getDesciption);
+            }
+            return CreateTodo();
+        }
+    }
+}
